58c18d98cbfcd1f0e16760e17608a5296282d364
Henry Chan <hchan@apache.org>
2014-05-09 23:08:30 -0700

    a

---
+++ src/com/bcit/a00788076/playGit/HelloWorld.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.bcit.a00788076.playGit;
+
+/**
+ * @author Henry
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
